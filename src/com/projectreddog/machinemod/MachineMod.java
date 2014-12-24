f5327281176ec25084fbfc8a564e149bfcba7848
TechStack <TechStackLP@gmail.com>
2014-12-23 21:39:33 -0500

    updated to handle configs in 1.7.10 within the game

--- src/com/projectreddog/machinemod/MachineMod.java
+++ src/com/projectreddog/machinemod/MachineMod.java
@@ -1,16 +1,18 @@
 package com.projectreddog.machinemod;
 
-import com.projectreddog.machinemod.configuration.ConfigurationHandler;
+import com.projectreddog.machinemod.handler.ConfigurationHandler;
 import com.projectreddog.machinemod.proxy.IProxy;
 import com.projectreddog.machinemod.reference.Reference;
+import com.projectreddog.machinemod.utility.LogHelper;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod (modid= Reference.MOD_ID, name= Reference.MOD_NAME,version=Reference.VERSION)
+@Mod (modid= Reference.MOD_ID, name= Reference.MOD_NAME,version=Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class MachineMod {
 
 	@SidedProxy(clientSide=Reference.CLIENT_PROXY_CLASS,serverSide=Reference.SERVER_PROXY_CLASS)
@@ -24,6 +26,8 @@ public class MachineMod {
 	//net handling mod config init items & blocks	
 		
 		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+		FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+		
 	}
 	@Mod.EventHandler 
 	public void init(FMLInitializationEvent event){
@@ -33,5 +37,7 @@ public class MachineMod {
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event){
 		// wrap things up .. runs after other mods do there init steps
+		
+		LogHelper.debug("Is Bulldozer Enabled: " + Reference.enableBulldozer);
 	}
 }
--- src/com/projectreddog/machinemod/reference/Reference.java
+++ src/com/projectreddog/machinemod/reference/Reference.java
@@ -8,6 +8,9 @@ public class Reference {
 	public static final String VERSION = "1.7.10-1.0-Pre-ALPHA";
 	public static final String CLIENT_PROXY_CLASS = "com.projectreddog.machinemod.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.projectreddog.machinemod.proxy.ServerProxy";
+	public static final String GUI_FACTORY_CLASS = "com.projectreddog.machinemod.client.gui.GuiFactory";
 	
+	
+	// config file settings
 	public static	boolean enableBulldozer=true;
 }
